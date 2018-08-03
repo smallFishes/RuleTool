3bd08c923f4bf14259d0f5560b397ba737d88cf1
zhaojun0193 <zhaojun0193@163.com>
2018-08-03 11:12:12 +0800

    修复bug
    修复初次安装没有默认配置的问题

--- js/background.js
+++ js/background.js
@@ -1,6 +1,33 @@
 /**
  * Created by hasee on 2018/7/31.
  */
-$(function () {
-    console.log("gou")
+options = {
+    "login_setting":{
+        "uat":{
+            "username":"adminuat",
+            "password":"admin_engine",
+            "isAutoLogin":false,
+            "isAutoCode":true
+        },
+        "engine":{
+            "username":"admin",
+            "password":"admin_engine",
+            "isAutoLogin":false,
+            "isAutoCode":true
+        }
+    },
+    "website":{
+        "uat_site":true,
+        "engine_site":true,
+        "uat_rts":false,
+        "engine_rts":false
+    }
+};
+
+$(function(){
+    chrome.storage.local.get('rule_options',function(result){
+        if(!result.rule_options){
+            chrome.storage.local.set({rule_options: options});
+        }
+    });
 });
